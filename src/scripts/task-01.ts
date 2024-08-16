1df51853f1a84994419b7a0b7f2c47bbfaae6af5
Denys Strashniuk <strashnyuk.denis@icloud.com>
2024-08-16 11:58:06 +0200

    add task-01

--- src/scripts/task-01.ts
+++ src/scripts/task-01.ts
@@ -1,4 +1,32 @@
-console.log("task-01");
-console.log("");
+console.log('task-01');
+console.log('');
+
+type Account = { login: string; email: string };
+
+function Account(this: any, { login, email }: Account) {
+  this.login = login;
+  this.email = email;
+
+  this.getInfo = function () {
+    console.log(`Login: ${this.login}, Email: ${this.email}
+`);
+  };
+}
+
+console.log(Account.prototype.getInfo); // function
+
+const mango = new (Account as any)({
+  login: 'Mangozedog',
+  email: 'mango@dog.woof',
+});
+
+mango.getInfo(); // Login: Mangozedog, Email: mango@dog.woof
+
+const poly = new (Account as any)({
+  login: 'Poly',
+  email: 'poly@mail.com',
+});
+
+poly.getInfo(); // Login: Poly, Email: poly@mail.com
 
 export {};
