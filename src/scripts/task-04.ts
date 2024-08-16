8495f2e0e09d037ae462dfe9df2cea0933cca2f6
Denys Strashniuk <strashnyuk.denis@icloud.com>
2024-08-16 13:12:22 +0200

    add task-04

--- src/scripts/task-04.ts
+++ src/scripts/task-04.ts
@@ -1,4 +1,39 @@
-console.log("task-01");
-console.log("");
+console.log('task-04');
+console.log('');
+
+class StringBuilder {
+  private _value: string;
+
+  constructor(initialValue: string) {
+    this._value = initialValue;
+  }
+
+  get value(): string {
+    return this._value;
+  }
+
+  append(str: string): void {
+    this._value += str;
+  }
+
+  prepend(str: string): void {
+    this._value = str + this._value;
+  }
+
+  pad(str: string): void {
+    this._value = str + this._value + str;
+  }
+}
+
+const builder = new StringBuilder('.');
+
+builder.append('^');
+console.log(builder.value); // '.^'
+
+builder.prepend('^');
+console.log(builder.value); // '^.^'
+
+builder.pad('=');
+console.log(builder.value); // '=^.^='
 
 export {};
