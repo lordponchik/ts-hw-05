3c3a0d516ce49fff4ea398083cfbef38ff70a7cb
Denys Strashniuk <strashnyuk.denis@icloud.com>
2024-08-16 12:55:26 +0200

    add task-03

--- src/scripts/task-03.ts
+++ src/scripts/task-03.ts
@@ -1,4 +1,44 @@
-console.log("task-01");
-console.log("");
+console.log('task-03');
+console.log('');
+
+interface IStorage {
+  storage: string[];
+  getItems(): string[];
+  addItem(item: string): void;
+  removeItem(item: string): void;
+}
+
+class Storage implements IStorage {
+  storage: string[];
+
+  constructor(storage: string[]) {
+    this.storage = storage;
+  }
+
+  getItems(): string[] {
+    return this.storage;
+  }
+
+  addItem(item: string): void {
+    this.storage.push(item);
+  }
+
+  removeItem(item: string): void {
+    if (this.storage.includes(item)) {
+      this.storage.splice(this.storage.indexOf(item), 1);
+    }
+  }
+}
+
+const storage = new Storage(['Нанитоиды', 'Пролонгер', 'Железные жупи', 'Антигравитатор']);
+
+const items = storage.getItems();
+console.table(items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор" ]
+
+storage.addItem('Дроид');
+console.table(items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор", "Дроид" ]
+
+storage.removeItem('Пролонгер');
+console.table(items); // [ "Нанитоиды", "Железные жупи", "Антигравитатор", "Дроид" ]
 
 export {};
